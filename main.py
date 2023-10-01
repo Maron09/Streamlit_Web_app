71c96ea6ff0da7b5cf31cd24c43788d6d239f2f2
maron09 <maroks216@gmail.com>
2023-10-01 21:07:30 +0100

    Updates

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import pandas as pd
+
+
+data = {
+    'Series_1': [1,2,3,4,7],
+    'Series_2': [10,20,30,100,250],
+}
+
+df = pd.DataFrame(data)
+
 
 st.title("Our Streamlit App")
 st.subheader("Introducing Streamlit In Automate Everything")
@@ -7,3 +17,10 @@ st.write(
     First web app
     Enjoy It!!
     """)
+
+st.write(df)
+st.line_chart(df)
+st.area_chart(df)
+
+my_slider = st.slider('Celsius')
+st.write(my_slider, 'in Fahrenheit is', my_slider * 9 / 5 + 32)
