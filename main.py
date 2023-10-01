c322bc07c954a1ee8b25e3bc5d4247a1835e94a5
maron09 <maroks216@gmail.com>
2023-10-01 20:26:25 +0100

    Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Our Streamlit App")
+st.subheader("Introducing Streamlit In Automate Everything")
+st.write(
+    """
+    First web app
+    Enjoy It!!
+    """)
